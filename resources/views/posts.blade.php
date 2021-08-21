fa3fff2e363118ab40db9f33e72efd112a1b0ed3
anythingultimate <52553493+anythingultimate@users.noreply.github.com>
2021-08-21 22:33:40 +0530

    The Basics- Store-Blog-Posts-As-HTML-Files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,21 +9,21 @@
 </head>
 <body class="bg-gray-50 ">
     <article class="px-3 mb-3 py-4">
-        <h1 class="text-3xl font-semibold"><a href="/post"> My First Post</a></h1>
+        <h1 class="text-3xl font-semibold"><a href="/posts/my-first-post"> My First Post</a></h1>
         <p class="pb-4">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
         <hr />
     </article>
     <article class="px-3 mb-3 py-4">
-        <h1 class="text-3xl font-semibold"><a href="/post">My Second Post</a></h1>
+        <h1 class="text-3xl font-semibold"><a href="/posts/my-second-post">My Second Post</a></h1>
         <p class="pb-4">
         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
         </p>
         <hr />
     </article>
     <article class="px-3 mb-3 py-4">
-        <h1 class="text-3xl font-semibold"><a href="/post">My Third Post</a></h1>
+        <h1 class="text-3xl font-semibold"><a href="/posts/my-third-post">My Third Post</a></h1>
         <p class="pb-4">
         It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
         </p>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,21 @@ Route::get('/', function () {
     return view('posts');
 });
 
-Route::get('post', function () {
-    return view('post');
+Route::get('posts/{post}', function ($slug) {
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+
+    if (!file_exists($path)) {
+
+        // dd('File does not exist');
+
+        // abort(404);
+
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
 });
