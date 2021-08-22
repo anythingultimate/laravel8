8188168ef1e6675b6d0dafd15b84c9cd011736b8
anythingultimate <52553493+anythingultimate@users.noreply.github.com>
2021-08-22 20:11:33 +0530

    Blade- Layout-Files-Two-Ways

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
+    <x-slot name="title">
+        {{$post->title}}
+    </x-slot>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Post</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-
-<body class="bg-gray-50 ">
     <article class="px-3 mb-3 py-4">
         <h1 class="text-3xl font-semibold">{{ $post->title }}</h1>
         <p class="pb-4">
@@ -17,7 +10,4 @@
         </p>
     </article>
     <a class="px-3" href="/"> Go Back</a>
-    <script src="{{asset('/js/app.js')}}"></script>
-</body>
-
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel 8</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body class="bg-gray-50 ">
+<x-layout>
     @foreach ($posts as $post)
-
-    <article class="px-3 mb-3 py-4">
-        <h1 class="text-3xl font-semibold"><a href="posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
-        <p class="pb-4">
-            {{ $post->excerpt }}
-        </p>
-        <hr />
-    </article>
+        <article class="px-3 mb-3 py-4">
+            <h1 class="text-3xl font-semibold"><a href="posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
+            <p class="pb-4">
+                {{ $post->excerpt }}
+            </p>
+            <hr />
+        </article>
     @endforeach;
-    <script src="{{asset('/js/app.js')}}"></script>
-</body>
-</html>
+</x-layout>
+
+
+
