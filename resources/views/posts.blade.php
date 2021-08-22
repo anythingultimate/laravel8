525ce53b5afc52733fe6d4b8c5d7f2e45a9b66ba
anythingultimate <52553493+anythingultimate@users.noreply.github.com>
2021-08-22 22:13:15 +0530

    Make-a-Post-Model-and-Migration

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     @foreach ($posts as $post)
         <article class="px-3 mb-3 py-4">
-            <h1 class="text-3xl font-semibold"><a href="posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
+            <h1 class="text-3xl font-semibold"><a href="posts/{{ $post->id }}">{{ $post->title }}</a></h1>
             <p class="pb-4">
                 {{ $post->excerpt }}
             </p>
